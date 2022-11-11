baad25b1d7a2aabad40d751f0fc2316dee8a55eb
Norman <normanndot@gmail.com>
2022-11-11 14:09:23 +0000

    Initial commit

---
+++ GraphQLExperiement/GraphQLExperiement/GraphQLExperiementApp.swift
@@ -0,0 +1,17 @@
+//
+//  GraphQLExperiementApp.swift
+//  GraphQLExperiement
+//
+//  Created by Norman D on 11/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct GraphQLExperiementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
