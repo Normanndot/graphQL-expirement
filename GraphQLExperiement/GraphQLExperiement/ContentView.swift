caa6e546bc42a15713428b4b16782943dd662886
Norman <normanndot@gmail.com>
2022-11-18 22:06:34 +0000

    Add 0.53.0 SPM of Apollo Client Added the Run Script Re-arrange as per documentation Added Sample Schema Added Sample Query Build the App. 🎉 API.swift file will be generated.

--- GraphQLExperiement/GraphQLExperiement/ContentView.swift
+++ GraphQLExperiement/GraphQLExperiement/ContentView.swift
@@ -7,20 +7,65 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+struct Message: Decodable, Identifiable {
+    let id: Int
+    let user: String
+    let text: String
 }
 
+// Our content view is able to handle two kinds of message box now.
+struct ContentView: View {
+    @State private var messages = [Message]()
+    @State private var selectedBox = "Inbox"
+    let messageBoxes = ["Inbox", "Sent"]
+
+    var body: some View {
+        NavigationView {
+            List {
+                Section {
+                    ForEach(messages) { message in
+                        VStack(alignment: .leading) {
+                            Text(message.user)
+                                .font(.headline)
+
+                            Text(message.text)
+                        }
+                    }
+                }
+            }
+            .listStyle(.insetGrouped)
+            .navigationTitle(selectedBox)
+
+            // Our task modifier will recreate its fetchData() task whenever selectedBox changes
+            .task(id: selectedBox) {
+                await fetchData()
+            }
+            .toolbar {
+                // Switch between our two message boxes
+                Picker("Select a message box", selection: $selectedBox) {
+                    ForEach(messageBoxes, id: \.self, content: Text.init)
+                }
+                .pickerStyle(.segmented)
+            }
+        }
+    }
+
+    // This is almost the same as before, but now loads the selectedBox JSON file rather than always loading the inbox.
+    func fetchData() async {
+        do {
+            let url = URL(string: "https://hws.dev/\(selectedBox.lowercased()).json")!
+            let (data, _) = try await URLSession.shared.data(from: url)
+            messages = try JSONDecoder().decode([Message].self, from: data)
+        } catch {
+            messages = [
+                Message(id: 0, user: "Failed to load message box.", text: "Please try again later.")
+            ]
+        }
+    }
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
